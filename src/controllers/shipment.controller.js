071a1d78d44f040baa4a179167f6411b904ca1c6
ManojDhundhalva <manojdhundhalva60@gmail.com>
2025-09-09 13:24:35 +0530

    API 2: Add Hop to an Existing Route Completed

--- src/controllers/shipment.controller.js
+++ src/controllers/shipment.controller.js
@@ -1,10 +1,4 @@
-// {
-//   "origin": "Ahmedabad", (required)
-//   "destination": "New York",(required)
-//   “shipment_number”: ‘12345’ (required)
-// }
-
-const shipments = require("../data/shipment");
+let shipments = require("../data/shipment");
 
 const createShipment = (req, res) => {
 
@@ -20,6 +14,8 @@ const createShipment = (req, res) => {
         return res.status(400).json({ success: false, message: "Origin and destination are required fields." });
     }
 
+    // can check, if already exist shipment_number
+
     const shipment = {
         shipment_number,
         hops: [origin, destination]
@@ -30,4 +26,42 @@ const createShipment = (req, res) => {
     return res.status(201).json({ success: true, message: "Shipment created successfully.", data: shipment });
 };
 
-module.exports = { createShipment };
+const addHop = (req, res) => {
+
+    const data = req.body;
+
+    const shipment_number = req.params.shipment_number;
+
+    if(!data?.previous_hop || !data?.next_hop || !data?.new_hop) {
+        return res.status(400).json({ success: false, message: "Previous hop, next hop and new hop are required fields." });
+    }
+
+    const { previous_hop, next_hop, new_hop } = data;
+
+    if(previous_hop.trim() === "" || next_hop.trim() === "" || new_hop.trim() === "") {
+        return res.status(400).json({ success: false, message: "Previous hop, next hop and new hop are required fields." });
+    }
+
+    const isExist = shipments.find((shipment) => shipment.shipment_number == shipment_number);
+
+    if(!isExist) {
+        return res.status(404).json({ success: false, message: "Shipment with ID not found."});
+    }
+
+    let new_shipment = {};
+
+    shipments = shipments.map((shipment) => {
+      if (shipment.shipment_number === shipment_number) {
+        shipment.hops.splice(shipment.hops.indexOf(next_hop), 0, new_hop);
+
+        new_shipment = { shipment_number: shipment.shipment_number, hops: shipment.hops }
+
+        return shipment;
+      }
+      return shipment;
+    });
+
+    return res.status(201).json({ success: true, message: "Hop added successfully.", data: new_shipment });
+};
+
+module.exports = { createShipment, addHop };
--- src/routes/shipments.routes.js
+++ src/routes/shipments.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { createShipment } = require('../controllers/shipment.controller');
+const { createShipment, addHop } = require('../controllers/shipment.controller');
 
 router.post('/create', createShipment);
+router.post('/:shipment_number/hops/add', addHop);
 
 module.exports = router;
